b1ed8f3760970069f30bf5669b587031949339d4
Martin Todorov <carlspring@gmail.com>
2012-04-04 19:05:41 +0300

    Working recursive mode.

---
+++ src/test/java/org/carlspring/maven/relocation/RelocateRecursiveMojoTest.java
@@ -0,0 +1,60 @@
+package org.carlspring.maven.relocation;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+
+import java.io.File;
+
+/**
+ * @author mtodorov
+ */
+public class RelocateRecursiveMojoTest
+        extends AbstractMojoTestCase
+{
+
+    public static final String DIR_TEST_CLASSES = new File(System.getProperty("basedir") + "/target/test-classes").getAbsolutePath();
+
+    private static final String POM_PLUGIN = DIR_TEST_CLASSES + "/poms/pom-relocate-single.xml";
+
+    public static final String ORIGINAL_GAV = "com.foo.bar:zoo:1.1";
+
+    public static final String RELOCATION_GAV = "com.foo.bars:zoo:1.1";
+
+    RelocateSingleMojo mojo;
+
+
+    @Override
+    public void setUp()
+            throws Exception
+    {
+        super.setUp();
+
+        mojo = (RelocateSingleMojo) lookupMojo("relocate-single", POM_PLUGIN);
+        mojo.setRepositoryBaseDir(DIR_TEST_CLASSES + "/local-repo");
+        mojo.setOriginalGAV(ORIGINAL_GAV);
+        mojo.setRelocationGAV(RELOCATION_GAV);
+        mojo.setRelocationMessage("The artifact has been relocated from com.foo.bar:zoo:1.1 to com.foo.bars:zoo:1.1.");
+    }
+
+    public void testSingleRelocation()
+            throws MojoExecutionException, MojoFailureException
+    {
+        mojo.execute();
+
+        assertTrue("Failed to backup the artifacts!", new File(DIR_TEST_CLASSES + "/local-repo/com/foo/bar/zoo/1.1/backup").exists());
+
+        assertTrue("Failed to preserve the artifacts which should not be removed!", new File(DIR_TEST_CLASSES + "/local-repo/com/foo/bar/zoo/1.1/zoo-1.1.pom").exists());
+        assertTrue("Failed to preserve the artifacts which should not be removed!", new File(DIR_TEST_CLASSES + "/local-repo/com/foo/bar/zoo/1.1/zoo-1.1.pom.md5").exists());
+        assertTrue("Failed to preserve the artifacts which should not be removed!", new File(DIR_TEST_CLASSES + "/local-repo/com/foo/bar/zoo/1.1/zoo-1.1.pom.sha1").exists());
+
+        assertFalse("Failed to delete the artifacts!", new File(DIR_TEST_CLASSES + "/local-repo/com/foo/bar/zoo/1.1/zoo-1.1.jar").exists());
+        assertFalse("Failed to delete the artifacts!", new File(DIR_TEST_CLASSES + "/local-repo/com/foo/bar/zoo/1.1/zoo-1.1.jar.md5").exists());
+        assertFalse("Failed to delete the artifacts!", new File(DIR_TEST_CLASSES + "/local-repo/com/foo/bar/zoo/1.1/zoo-1.1.jar.sha1").exists());
+
+        // TODO:
+        // TODO: Check that the newly generated .pom file for the relocated artifact contains the proper GAV
+        // TODO:
+    }
+
+}
